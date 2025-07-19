9dda2d24da0bcbdb327685ffd25ab565206cddc8
夏风 <1758840627@qq.com>
2025-07-20 03:03:22 +0800

    解决冲突之后的代码

--- pin03.c
+++ pin03.c
@@ -9,10 +9,8 @@ int main()
 {
     printf("this for test the test-demo\r\n");
     printf(" test-demo and is  03333\r\n");
-<<<<<<< HEAD
     printf(" 这是dev分支修改的代码\r\n");
-=======
+
     printf("test分支修改的代码");
->>>>>>> test
     return 0;
 }
